c966b6db640ee60ff7db4acdf66cf4e416f0f3d5
siwiakmichal <siwiakmichal@gmail.com>
2018-09-24 23:55:58 +0200

    updated frond end

--- src/main/resources/static/scripts/angular/functions.js
+++ src/main/resources/static/scripts/angular/functions.js
@@ -93,9 +93,7 @@ app.controller('MainController', function ($scope, $http) {
                     1442912400000, 1442916000000,
                     1442919600000, 1442923200000,
                     1442926800000, 1442930400000,
-                    1442934000000, 1442937600000,
-                    1442941200000, 1442944800000,
-                    1442948400000
+                    1442934000000, 1442937600000
                 ],
                 lineColor: "white",
                 lineWidth: "1px",
@@ -150,46 +148,12 @@ app.controller('MainController', function ($scope, $http) {
                 text: "All Sites",
                 values: [2596, 2626, 4480,
                     6394, 7488, 14510,
-                    7012, 10389, 20281,
-                    25597, 23309, 22385,
-                    25097, 20813, 20510
+                    7012, 10389, 40000,
+                    50000
                 ],
                 backgroundColor1: "#77d9f8",
                 backgroundColor2: "#272822",
                 lineColor: "#40beeb"
-            }, {
-                text: "Site 1",
-                values: [479, 199, 583,
-                    1624, 2772, 7899,
-                    3467, 3227, 12885,
-                    17873, 14420, 12569,
-                    17721, 11569, 7362
-                ],
-                backgroundColor1: "#4AD8CC",
-                backgroundColor2: "#272822",
-                lineColor: "#4AD8CC"
-            }, {
-                text: "Site 2",
-                values: [989, 1364, 2161,
-                    2644, 1754, 2015,
-                    818, 77, 1260,
-                    3912, 1671, 1836,
-                    2589, 1706, 1161
-                ],
-                backgroundColor1: "#1D8CD9",
-                backgroundColor2: "#1D8CD9",
-                lineColor: "#1D8CD9"
-            }, {
-                text: "Site 3",
-                values: [408, 343, 410,
-                    840, 1614, 3274,
-                    2092, 914, 5709,
-                    6317, 6633, 6720,
-                    6504, 6821, 4565
-                ],
-                backgroundColor1: "#D8CD98",
-                backgroundColor2: "#272822",
-                lineColor: "#D8CD98"
             }]
         }
     };
